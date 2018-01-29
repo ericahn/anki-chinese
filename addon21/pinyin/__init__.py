4d55e6adf4165f4bafeb52189bcdd2f83ceb4393
Eric Ahn <ericahn0912@gmail.com>
2018-01-29 11:07:47 +0000

    Move all logic to Chinese Master

--- addon21/pinyin/__init__.py
+++ addon21/pinyin/__init__.py
@@ -2,37 +2,20 @@ import os
 import codecs
 
 try:
-    import jieba
-    jieba.setLogLevel(60)
-except ImportError:
-    jieba_import = False
-else:
-    jieba_import = True
-
-try:
     # import the main window object (mw) from aqt
     from aqt import mw
     # import the "show info" tool from utils.py
     from aqt.utils import showInfo
     # import all of the Qt GUI library
     from aqt.qt import *
+    from .gui import *
 except ImportError:
     print('Failed to import Anki modules')
+    mw = None
 
-from .chinese_dict import ChineseDict
-from .html import generate_ruby, generate_definitions_table
-from .gui import *
+from .chinese_master import ChineseMaster
 
 module_path = os.path.join(os.path.dirname(__file__), '..', 'user_files')
-
-cedict_path = os.path.join(module_path, 'cedict_ts.u8')
-cedict = ChineseDict(codecs.open(cedict_path, 'r', 'utf-8'))
-
-if jieba_import:
-    jieba_path = os.path.join(module_path, 'jieba_extra.u8')
-    jieba.load_userdict(jieba_path)
-
-env = None
 
 if mw:
     chinese_menu = None
@@ -46,5 +29,5 @@ if mw:
         mw.form.menuTools.addMenu(chinese_menu)
 
     action = QAction("Pinyin helper", mw)
-    action.triggered.connect(lambda: MainMenu(env))
+    action.triggered.connect(lambda: MainMenu(ChineseMaster(mw, module_path)))
     chinese_menu.addAction(action)
